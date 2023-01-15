942cb6524de55ce5e542b6a53e40b39fb476f9f8
Wesley Vos <wesley.vos@icloud.com>
2023-01-15 19:42:08 +0100

    Adjust scan interval

--- custom_components/rootedtoon/coordinator.py
+++ custom_components/rootedtoon/coordinator.py
@@ -3,17 +3,16 @@ from __future__ import annotations
 
 from datetime import timedelta
 import logging
-import secrets
 
 from rootedtoonapi import Devices, Toon, ToonError
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,8 +26,8 @@ class RootedToonDataUpdateCoordinator(DataUpdateCoordinator[Devices]):
         self.config = entry.data
 
         self.toon = Toon(
-            host=self.config[CONF_HOST],
-            port=self.config[CONF_PORT],
+            host=self.config.get(CONF_HOST),
+            port=self.config.get(CONF_PORT),
             session=async_get_clientsession(hass),
         )
 
@@ -36,7 +35,7 @@ class RootedToonDataUpdateCoordinator(DataUpdateCoordinator[Devices]):
             hass,
             _LOGGER,
             name=DOMAIN,
-            update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
+            update_interval=timedelta(seconds=self.config.get(CONF_SCAN_INTERVAL)),
         )
 
     async def _async_update_data(self) -> Devices:
